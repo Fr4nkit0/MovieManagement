098c0f0b7bfcd622fd14b29fe6c73323c12bb05c
Franco Calisaya <calisaya.frnc@gmail.com>
2024-07-29 09:34:22 -0300

    Iniciando el proyecto

---
+++ src/test/java/frankito/net/NetApplicationTests.java
@@ -0,0 +1,13 @@
+package frankito.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
